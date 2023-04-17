67b9ce712993d43290b7b36b7c241dc1ae24b559
Mario <mariomatheuspombal@rede.ulbra.br>
2023-04-17 19:30:42 -0300

    Correção back-end

--- back-end/cityguard/src/api/ocorrencia/services/ocorrencia.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * ocorrencia service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::ocorrencia.ocorrencia');
